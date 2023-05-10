52a29592b27452f47b8e70551bb2485f8981299d
Ralf <ralf.stockreiter@posteo.de>
2023-05-10 22:10:46 +0200

    Übung 07

--- Uebung07/Main.cpp
+++ Uebung07/Main.cpp
@@ -1,8 +1,23 @@
 #include "Table.h"
-
 #include <iostream>
 
-int main() {
+using namespace std;
 
+int main() {
+	
+	Table<int> t1;
+	
+	try {
+		t1.insert(4);
+		t1.append(5);
+		t1.append(6);
+		cout << "Current record: " << t1.get_record() << endl;
+		t1.delete_record();
+		cout << "Current record: " << t1.get_record() << endl;
+		t1.next();
+	}
+	catch (out_of_range& e) {
+		cerr << "Exception thrown: " << e.what() << endl;
+	}
 	return 0;
 }
--- Uebung07/Table.h
+++ Uebung07/Table.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdexcept>
+
+using namespace std;
+
 template <class item_type>
 class Table {
 	struct record {
@@ -23,24 +27,22 @@ public:
 	void insert(item_type r) { // fügt neuen Record vor dem aktuellen Record ein
 		record* temp = iterator;
 		record* rec = new record;
-
-		if (temp->next != nullptr) {	//next record exists
-			temp->next->prev = rec;
-			rec->next = temp->next;
-		}
-		else {
+		
+		if (iterator == nullptr) {			//no record exists
 			rec->next = nullptr;
 			tail = rec;
-		}
-		if (iterator != nullptr) {		//prev record exists
-			temp->next = rec;
-			rec->prev = temp;
-		}
-		else {
 			rec->prev = nullptr;
 			head = rec;
 		}
-
+		else if (temp->next != nullptr) {	//next record exists
+			temp->next->prev = rec;
+			rec->next = temp->next;
+		}
+		else if (temp->next = nullptr) {	//next record doesn't exist
+			temp->next = rec;
+			rec->prev = temp;
+		}
+		
 		rec->item = r;
 		iterator = rec;
 	}
@@ -53,25 +55,59 @@ public:
 		rec->next = nullptr;
 		tail = rec;
 
+		rec->item = r;
 		iterator = rec;
 	}
 	void first() { // erster Record wird zum aktuellen Record
+		if (iterator == nullptr) {
+			throw out_of_range("empty stack");
+		}
 		iterator = head;
 	}
 	void last() { // letzter Record wird zum aktuellen Record
+		if (iterator == nullptr) {
+			throw out_of_range("empty stack");
+		}
 		iterator = tail;
 	}
 	void next() { // der dem aktuellen Record folgende Record wird zum neuen aktuellen Record
+		if (iterator->next == nullptr) {
+			throw out_of_range("no next item available");
+		}
 		iterator = iterator->next;
 	}
 	void previous() { // der dem aktuellen Record vorausgehende Record wird zum neuen aktuellen Record
+		if (iterator->prev == nullptr) {
+			throw out_of_range("no previous item available");
+		}
 		iterator = iterator->prev;
 	}
 	void delete_record() { // löscht aktuellen Record
+		record* rec = iterator;
 		
+		if (iterator == nullptr) {
+			throw out_of_range("empty stack");
+		}
+		else if (iterator->next != nullptr && iterator->prev != nullptr) {	//both prev and next record exist
+			iterator->next->prev = iterator->prev;
+			iterator->prev->next = iterator->next;
+			iterator = iterator->prev;
+		}
+		else if (iterator->next != nullptr && iterator->prev == nullptr) {	//next record exists, prev record doesn't exist
+			iterator->next->prev = nullptr;
+			iterator = iterator->next;
+		}
+		else if (iterator->next == nullptr && iterator->prev != nullptr) {	//next record doesn't exist, prev record exists
+			iterator->prev->next = nullptr;
+			iterator = iterator->prev;
+		}
+		else if (iterator->next == nullptr && iterator->prev == nullptr) {	//both prev and next record don't exist
+			iterator = nullptr;
+		}
+
+		delete rec;
 	}
-	item_type get_record(item_type* r) { // liefert aktuellen Record in der Recordvariablen r
-		r = iterator->item;
+	item_type get_record() { // liefert aktuellen Record in der Recordvariablen r
 		return iterator->item;
 	}
 	void set_record(item_type& r) { // ersetzt aktuellen Record durch Recordvariable r	
